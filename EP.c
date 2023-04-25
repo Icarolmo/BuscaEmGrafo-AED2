54df25466ba93273263ebd08bfb43933ec5049b2
Icarus <72323389+Icarolmo@users.noreply.github.com>
2023-04-25 14:10:50 +0000

    Criação do arquivo.c e ajustes pontuais.

---
+++ EP.c
@@ -0,0 +1,101 @@
+//--------------------------------------------------------------
+// NOMES DOS RESPONSÁVEIS: Icaro Lima de Oliveira E Wesley Fernandes
+//--------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int grupo() {
+  return 10;
+}
+
+// ######### ESCREVA O NROUSP AQUI
+int nroUSP1() {
+    return 13718662;
+}
+
+int nroUSP2() {
+    return 13716893;
+}
+
+// elemento das listas de adjacência e de resposta - NAO ALTERE ESTA DEFINICAO
+typedef struct estr 
+{
+    int adj; // elemento
+	int peso; // custo (não precisa ser usado na resposta)
+    struct estr *prox;
+} NO;
+
+// vertices do grafo (salas) - use este tipo ao criar o grafo  - NAO ALTERE ESTA DEFINICAO
+typedef struct 
+{
+    int flag; // para uso na busca em largura e profundidade, se necessario
+    int aberto; // vale true se a sala em questao esta aberta
+    int via; // use este campo se precisar computar algum caminho etc.
+	int dist; // use este campo se precisar computar alguma distância etc.
+    NO* inicio;
+} VERTICE;
+
+
+// A PARTIR DAQUI IMPLEMENTAMOS NOSSOS CÓDIGOS 
+
+void abrirSalas(int *aberto){ // Função para abertura de todas as salas (somente caso encontre a chave)
+	
+	int tamanho = sizeof(aberto)/sizeof(int); // Calculo do tamanho do vetor
+	int i = 0;	
+	while(i<tamanho){
+		aberto[i] = 1;	
+	}
+}
+
+
+// funcao principal
+NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int chave);
+
+//------------------------------------------
+// O EP consiste em implementar esta funcao
+// e outras funcoes auxiliares que esta
+// necessitar
+//------------------------------------------
+NO *caminho(int N, int A, int *ijpeso, int *aberto, int inicio, int fim, int chave)
+{
+	NO* resp;
+	resp = NULL;
+
+	// seu codigo AQUI
+
+	//...
+
+	return resp;
+}
+
+
+
+//---------------------------------------------------------
+// use main() para fazer chamadas de teste ao seu programa
+//---------------------------------------------------------
+int main() {
+
+
+	// aqui vc pode incluir codigo de teste
+
+	// exemplo de teste trivial
+
+	int N=3; // grafo de 3 vértices numerados de 1..3
+    int A=3;
+	int aberto[] = {1,1,1}; // todos abertos
+	int inicio=1;
+	int fim=3;
+	int chave=2;
+	int ijpeso[]={1,2,10, 2,3,20, 3,1,10};
+
+	// o EP sera testado com uma serie de chamadas como esta
+	NO* teste = NULL;
+	teste = caminho(N, A, ijpeso, aberto, inicio, fim, chave);
+	return teste;
+
+}
+
+// por favor nao inclua nenhum código abaixo da função main()
+

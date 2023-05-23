805f855a6078a9bea0ea1453981efa0cc137113e
Holdrulff <wesley.fernan1@gmail.com>
2023-05-23 20:13:27 -0300

    Carregando arquivo grafosParaTestes.c contendo exemplos de grafos que utilizaremos nos nossos testes do ep

---
+++ grafosParaTestes.c
@@ -0,0 +1,67 @@
+int grafo1()
+{
+    int N = 6;
+    int A = 8;
+    int aberto[] = {1, 1, 1, 1, 1, 0};
+    int inicio = 1;
+    int fim = 5;
+    int chave = 2;
+    int ijpeso[] = {
+        1, 2, 14,
+        1, 6, 3,
+        1, 4, 5,
+        2, 3, 6,
+        2, 6, 5,
+        3, 4, 8,
+        4, 5, 10,
+        5, 6, 8};
+    return 0;
+}
+
+int grafo2()
+{
+    int N;
+    int A;
+    int aberto[];
+    int inicio;
+    int fim;
+    int chave;
+    int ijpeso[];
+    return 0;
+}
+
+int grafo3()
+{
+    int N;
+    int A;
+    int aberto[];
+    int inicio;
+    int fim;
+    int chave;
+    int ijpeso[];
+    return 0;
+}
+
+int grafo4()
+{
+    int N;
+    int A;
+    int aberto[];
+    int inicio;
+    int fim;
+    int chave;
+    int ijpeso[];
+    return 0;
+}
+
+int grafo5()
+{
+    int N;
+    int A;
+    int aberto[];
+    int inicio;
+    int fim;
+    int chave;
+    int ijpeso[];
+    return 0;
+}
